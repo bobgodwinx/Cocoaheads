2965cb53517ef33801071a2e9e00b8171b3ae070
Bob Godwin <bobgodwinx@gmail.com>
2023-03-09 12:58:12 +0100

    Initial Commit

---
+++ Cocoaheads/CocoaheadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocoaheadsApp.swift
+//  Cocoaheads
+//
+//  Created by Bob Godwin Obi on 09.03.23.
+//
+
+import SwiftUI
+
+@main
+struct CocoaheadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
